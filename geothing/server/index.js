b335ae807295cc11ca66451742f9ed9ec6f093a9
Samiksusamik <sam.kipnis@gmail.com>
2023-12-04 10:15:36 -0500

    e

--- geothing/server/index.js
+++ geothing/server/index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-
-import router from './router';
-
-app.use('/', router);
+import getCountryByCoords from './api/getCountryByCoords';
 
 const app = express();
 const port = 8080;
+
+const corsOptions = {
+	origin: 'http://localhost:5173',
+	optionsSuccessStatus: 200,
+};
+
+app.get('/api/getCountryByCoords', cors(corsOptions), async (req, res) => {
+	const country = await getCountryByCoords();
+	res.json(country);
+});
 
 app.listen(port, () => {
 	console.log('Server running on port', port);
