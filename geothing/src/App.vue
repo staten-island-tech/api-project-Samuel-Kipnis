9f903855f19ce24e1e074917b49a52b9c13f1cc8
Samiksusamik <sam.kipnis@gmail.com>
2023-12-08 10:12:36 -0500

    Update App.vue

--- geothing/src/App.vue
+++ geothing/src/App.vue
@@ -1,16 +1,24 @@
 <script setup>
-	// fetch('/').then((res) => {
-	// 	console.log(res);
-	// });
 	function getData() {
 		fetch('http://localhost:8080/api/getCountryByCoords')
 			.then((res) => res.text())
 			.then((text) => console.log(text));
 	}
+
+	function mouseMove(e) {
+		if (e.target.tagName === 'IMG') {
+			let map = e.target;
+			let mapRect = map.getBoundingClientRect();
+			console.log(mapRect);
+			let xToLongRatio = mapRect.width / 180;
+			console.log(e.clientX / xToLongRatio);
+		}
+	}
 </script>
 
 <template>
-	<div>
+	<div @mousemove="mouseMove">
 		<h1 @click="getData">Hello</h1>
+		<img src="https://geology.com/world/world-map.gif" alt="" id="map" />
 	</div>
 </template>
