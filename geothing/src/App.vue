92cf110a79026a8ecb59dc3bdb17ef04261f7e33
Samiksusamik <sam.kipnis@gmail.com>
2024-01-02 10:15:14 -0500

    Update App.vue

--- geothing/src/App.vue
+++ geothing/src/App.vue
@@ -7,7 +7,26 @@
 
 	import DataDisplay from './components/DataDisplay.vue';
 
-	const countryData = ref({});
+	const countryData = ref({
+		continent: 'NA',
+		capital: 'Washington',
+		languages: 'en-US,es-US,haw,fr',
+		geonameId: 6252001,
+		south: 24.543938895,
+		isoAlpha3: 'USA',
+		north: 49.3844873,
+		fipsCode: 'US',
+		population: '327167434',
+		east: -66.949185723,
+		isoNumeric: '840',
+		areaInSqKm: '9629091.0',
+		countryCode: 'US',
+		west: -124.73259849600001,
+		countryName: 'United States',
+		postalCodeFormat: '#####-####',
+		continentName: 'North America',
+		currencyCode: 'USD',
+	});
 
 	function getData(lat, lng) {
 		fetch(
@@ -19,11 +38,6 @@
 		)
 			.then((res) => res.json())
 			.then((data) => (countryData.value = data.geonames[0]));
-	}
-
-	function displayData(data) {
-		const output = document.querySelector('#output');
-		output.textContent = data;
 	}
 
 	function loadMap() {
@@ -42,8 +56,7 @@
 		}).addTo(map);
 
 		map.on('click', async (e) => {
-			const data = await getData(e.latlng.lat, e.latlng.lng);
-			displayData(data);
+			getData(e.latlng.lat, e.latlng.lng);
 		});
 	}
 
@@ -57,9 +70,10 @@
 		rel="stylesheet"
 		href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
 	<div id="map"></div>
-	<p>{{ countryData }}</p>
-	<h2 id="output">
-		{{ countryData.countryName }}, {{ countryData.continentName }}
+	<div id="load"></div>
+	<h2 id="output" :key="countryData">
+		{{ countryData.countryName }},
+		{{ countryData.continentName }}
 	</h2>
-	<h2 id="output">{{ Number(countryData.areaInSqKm) }}</h2>
+	<h2 id="output">{{ Number(countryData.areaInSqKm) || '' }}</h2>
 </template>
